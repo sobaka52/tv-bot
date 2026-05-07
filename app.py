e01da41cbaaa3d13b284f3aa12150639aeeccf47
aleks <alekszelenko2021@gmail.com>
2026-05-07 20:41:33 +0300

    fix webhook

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "WORKING"
+    return "Bot running"
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
     data = request.json
     print(data)
-    return {"ok": True}
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    return "ok", 200
